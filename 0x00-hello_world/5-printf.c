251deea72ef6b8aa710a941b13e373543c1d792e
impalainn <ahonavictor@gmail.com>
2022-09-11 02:47:18 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/** main.c */
+/**
+ * main - Entry point
+ * Description:'prints arguement'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
