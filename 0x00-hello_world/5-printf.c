3b883691ef0eb91c9c41226ded931da83bc39411
impalainn <ahonavictor@gmail.com>
2022-09-11 01:48:29 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int main(void)
+int main(void) /** main function */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0); /** returns to zero */
