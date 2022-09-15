d1551a980e4cd50058f449e994991d4b0f62fcda
impalainn <ahonavictor@gmail.com>
2022-09-15 05:01:13 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints arg'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+
+	while ('a' <= 'z')
+	putchar(alphabet);
+	alphabet++;
+	return (0);
+	}
