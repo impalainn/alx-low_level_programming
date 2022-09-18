cdfdc2c581a3d6d4a7b5020914fe299188766a38
impalainn <ahonavictor@gmail.com>
2022-09-18 02:30:10 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,12 +13,14 @@ for (x = 0; x < 10; x++)
 putchar(x + '0');
 }
 if (x > 9)
+{
 	putchar('a');
 	putchar('b');
 	putchar('c');
 	putchar('d');
 	putchar('e');
 	putchar('f');
+}
 putchar('\n');
 return (0);
 }
