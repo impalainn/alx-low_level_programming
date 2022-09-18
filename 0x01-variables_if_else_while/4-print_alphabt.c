673cb8ae55172ee77d75083426f28c1ad0c42601
impalainn <ahonavictor@gmail.com>
2022-09-18 01:07:52 -0700

    alphabetqe-

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ * Description: 'prints arg'
+ * Return: ALways 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+        alphabet != 'e' && 'q';
+
+		while (alphabet <= 'z')
+		{
+			putchar(alphabet);
+			alphabet++;
+		}
+	putchar('\n');
+	return (0);
+}
