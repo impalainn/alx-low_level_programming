579748a05590f4d1cd49f1b582cf6a211275c887
impalainn <ahonavictor@gmail.com>
2022-09-18 02:08:38 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-    int x;
+int x;
 
-    for (x = 0; x < 10; x++)
-    {
-        putchar(x + '0');
-    }
-    putchar('\n');
-    return (0);
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0');
+}
+putchar('\n');
+return (0);
 }
