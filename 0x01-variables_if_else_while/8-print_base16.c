5262f0ab71d8c73bc94aa125c076de2371f938a9
impalainn <ahonavictor@gmail.com>
2022-09-18 02:27:36 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,17 @@ int main(void)
 {
 int x;
 
-for (x = 0; x < 16; x++)
+for (x = 0; x < 10; x++)
 {
 putchar(x + '0');
 }
+if (x > 9)
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
 putchar('\n');
 return (0);
 }
