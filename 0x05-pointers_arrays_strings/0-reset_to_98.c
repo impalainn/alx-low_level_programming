74a8faa52de0a930637844d0b3cab04075b0b21a
impalainn <ahonavictor@gmail.com>
2022-09-21 00:48:23 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,37 @@
 #include "main.h"
+#include<limits.h>
 /**
- * main - updates value
- * Return: 0
+ * reset_to_98 - updates value
+ * @n: int variable
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
+}
+/**
+ * main - entry point
+ * Description: 'prints arg'
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
 
-        reset_to_98(n);
+	int y;
+
+	int z;
+
+	x = 402;
+	y = -4096;
+	z = INT_MAX;
+	printf("%d\n", x);
+	reset_to_98(&x);
+	printf("%d\n", x);
+	printf("%d\n", y);
+	reset_to_98(&y);
+	printf("%d\n", y);
+	printf("%d\n", z);
+	reset_to_98(&z);
+	printf("%d\n", z);
+	return (0);
 }
