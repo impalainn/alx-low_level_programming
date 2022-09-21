88c369836eec479f66c38927fb3e8bf9293457e3
impalainn <ahonavictor@gmail.com>
2022-09-21 09:16:20 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns strlen
+ * return: 0
+ */
+int _strlen(char *s)
+{
+{
+	char *s;
+}
+{
+	int len;
+
+	char str[] = "my first strlen";
+	len = _strlen(str);
+	printf("%d", len);
+}
+}
