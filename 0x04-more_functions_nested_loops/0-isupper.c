2fa01077fb9227789343812ef122f8a68489f539
impalainn <ahonavictor@gmail.com>
2022-09-22 04:26:10 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: integer
+ * Return: if c is uppercase, 1, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 91)
+		return (1);
+	else
+		return (0);
+}
