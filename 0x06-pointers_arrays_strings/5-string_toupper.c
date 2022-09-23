2a8875f18d38729284fb1413100dc570a4280c9c
impalainn <ahonavictor@gmail.com>
2022-09-22 18:52:29 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** string_toupper - transforms lowercase in string to uppercase
+ *
+ * @c: char string pointer
+ * Return: char pointer
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] > 96 && c[i] < 123)
+			c[i] -= 32;
+		i++;
+	}
+	return (c);
+}
