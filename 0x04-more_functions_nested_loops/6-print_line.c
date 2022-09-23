b570f2ae26ea4892675933138ffbc4f419825bb3
impalainn <ahonavictor@gmail.com>
2022-09-23 04:21:57 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints line line using char '_'
+ * @n: number for char '_'
+ */
+void print_line(int n)
+{
+		int i = 0;
+
+			if (n > 0)
+					{
+								while (i < n)
+											{
+															_putchar(95);
+																		i++;
+																				}
+									}
+				_putchar('\n');
+
