faed6273537aff6f4f823acd32a5ee01587d99e7
impalainn <ahonavictor@gmail.com>
2022-09-23 05:53:35 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - executes fizz-buzz test
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i < 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else
+			printf(" %d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+	
