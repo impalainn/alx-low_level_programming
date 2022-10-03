5759e06e8e65d0a9e365c082eb3e4f62ddc3041c
impalainn <ahonavictor@gmail.com>
2022-10-03 04:26:35 -0700

    primes

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_palindrome - checks palindrome string
+ * @s; string
+ * Return: 1 if palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (*s == " " || len == 1)
+		return (1);
+	else if 
+
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@ int is_prime_number(int n)
 {
 	if (!(n % 2 == 0 || n % 3 == 0))
 		return (1);
-	else if (n == 1 || n < 0)
+	else if (n == 1 || n <= 0)
 		return (0);
 	return (0);
 }
