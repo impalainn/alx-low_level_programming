f1bbe16268bf6747453268ad1c5432297e9c2f6a
impalainn <ahonavictor@gmail.com>
2022-10-05 00:27:29 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,19 +8,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum;
+	int i, k, sum;
 
-	int i;
-
-	if (argc > 1)
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!(atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9'))
+		for (k = 0; argv[i][k] != '\0'; k++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!((argv[i][k] >= '0' && argv[i][k] <= '9') || argv[i][k] == '-'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
 		}
-		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
