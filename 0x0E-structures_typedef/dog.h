74b44aec506bcf9172dcb1ddf499c6ff3e96d91b
impalainn <ahonavictor@gmail.com>
2022-10-11 09:10:20 -0700

    defining a new struct type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains elementys stated
+ * @name: element 1
+ * @age: element 2
+ * @owner: element 3
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
