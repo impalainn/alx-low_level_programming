82db0e6284ad4493d478ceb589dc5173dbef7b9d
impalainn <ahonavictor@gmail.com>
2023-03-20 00:20:02 -0700

    add betty styling

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,17 @@
 #include "main.h"
+/**
+ *print_last_digit -  print and return the last digit of an integer input
+ * @k:integer for which last digit is returned
+ * Return: last digit
+ */
 int print_last_digit(int k)
 {
-	int m = k%10;
+	int m = k % 10;
 
 	if (k < 0)
-        {
-              m = -m;
-        }
+	{
+		m = -m;
+}
 	_putchar('0' + m);
-	return m;
+	return (m);
 }
