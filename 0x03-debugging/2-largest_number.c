01148b96941920d6fe9c76dd3adc89cf0d44cb5b
impalainn <ahonavictor@gmail.com>
2023-08-05 23:04:21 -0700

    solve for a bug in largest number function

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,9 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (a > b && b > c)
+    else if (b > a && b > c)
     {
-	    largest = a;
-    }
-    else if (a > b && a == c)
-    {
-	    largest = c && a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
+	    largest = b;
     }
     else
     {
