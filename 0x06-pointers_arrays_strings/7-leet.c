c8d09dc78d33bcbaf19b90bd256f4448c15d54e3
impalainn <ahonavictor@gmail.com>
2023-08-17 17:29:49 -0700

    write code to transform letters to leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,6 +4,23 @@
  * @c: char string pointer
  * Return: char pointer
  */
-char *leet(char c)
+char *leet(char *c)
 {
+	int i = 0 , j;
+	char numbers[] = {'4', '3', '0', '7', '1'};
+	char letters[] = {'A', 'E', 'O', 'T', 'L'};
+
+	while (*(c + i) != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if(*(c + i) == letters[j] || *(c + i) - 32 == letters[j])
+			{
+				*(c + i) = numbers[j];
+			}
+		}
+		i++;
+	}
+
+	return (c);
 }
